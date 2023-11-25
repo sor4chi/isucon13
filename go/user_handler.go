b2597a7d9d22f85d27dd901379188bf34814016b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-11-25 13:21:49 +0900

    fix

--- go/user_handler.go
+++ go/user_handler.go
@@ -259,8 +259,8 @@ func registerHandler(c echo.Context) error {
 	// post request to powerdns
 	{
 		endpoint := "http://192.168.0.11:8081/api/v1/servers/localhost/zones/u.isucon.dev."
-		body := fmt.Sprintf(`{"rrsets": [{"name": "%s", "type": "A", "ttl": 3600, "changetype": "REPLACE", "records": [{"content": "%s", "disabled": false}]}]}`, req.Name, powerDNSSubdomainAddress)
-		req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(body))
+		body := fmt.Sprintf(`{"rrsets": [{"name": "%s.u.isucon.dev.", "type": "A", "ttl": 3600, "changetype": "REPLACE", "records": [{"content": "%s", "disabled": false}]}]}`, req.Name, powerDNSSubdomainAddress)
+		req, err := http.NewRequest(http.MethodPatch, endpoint, strings.NewReader(body))
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "failed to create request to powerdns: "+err.Error())
 		}
